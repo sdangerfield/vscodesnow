48a9fd580b5cff0f82f9d7f5db848b3182f5df6e
Stephen Dangerfield <dangerfield.stephen@yahoo.com.au>
2020-01-19 18:06:22 +1100

    fuck this rubbish

--- General-Utils/src/Server Development/Script Includes/vui_interaction.script.js
+++ General-Utils/src/Server Development/Script Includes/vui_interaction.script.js
@@ -1,10 +1,8 @@
+
 var vui_interaction = Class.create();
 // Creates a class to gradually enrich the inbound interaction
 // establish company, customer/user & account
 // then enrich with data events etc.
-
-
-
 vui_interaction.prototype = {
     
     
@@ -15,15 +13,16 @@ vui_interaction.prototype = {
         var sysUserID = '';                     //sysid from sys_user
         var sysAccountID = '';                  //sysid from account (if user cant be determined but account can)
         var e164Phone ='';                      //phone number in E164 format
-        var bSMS = fase;                        //bool can SMS
+        var bSMS = false;                        //bool can SMS
         var sysInteractionID = '';              //sysid from interactions table
-        var contactType = constContact.USER;    //const for contact type
-        var channel = constChannel.CALL;        //const for channel
+        var contactType = constContact.USER;    //const for contact type, default ot USER
+        var channel = constChannel.CALL;        //const for channel, default to call
         
         Object.freeze(this.constContact);       //freeze to make constants - loving Javascript
         Object.freeze(this.constChannel);
     },
     type: 'vui_interaction',
+
 
     constContact: {
         USER:   'user',
@@ -31,15 +30,37 @@ vui_interaction.prototype = {
         CUSTOMER: 'customer',
         CONTACT: 'contact'
     },
-
+    
     constChannel: {
         CALL: 'call',
         CHAT: 'chat',
         EMAIL: 'email',
         ASSISTANT: 'assistant'
     },
-    
+
 };
+
+
+var vui_event = Class.Create();
+vui_eventType.prototype = {
+    initialize: function() {
+        var intEventID = 0;                     //EventID of the Event
+        var strTitle ='';                        //Title of the Event    
+        var strDetail ='';                      //Detail of the Event
+        var constSource = constEventType.SYSTEM;
+        Object.freeze(this.constEventType);
+    },
+
+    type: 'vui_event',
+    constEventType: {
+        SYSTEM:  'System',
+        APPLICATION: 'Application',
+        SECURITY: 'Security',
+        PERFORMANCE: 'Performance',
+        CUSTOM: 'Custom'
+    },
+};
+
 
 
 
